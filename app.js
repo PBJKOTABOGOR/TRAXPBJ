9f6e3c1c243068d87f6c635a53d340cd81c884a0
PBJKOTABOGOR <pbjkotabogor@gmail.com>
2026-05-03 09:22:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -115,14 +115,19 @@ const APP_ROUTES = {
   }
 ,
 
-  'rapor-pbj-input-internal': {
-    title: 'Input Rapor PBJ',
-    subtitle: 'Form internal pegawai untuk input dan upload dokumen Rapor PBJ.',
-    type: 'module',
-    html: 'modules/rapor-input/rapor-input.html',
-    css: 'modules/rapor-input/rapor-input.css',
-    js: 'modules/rapor-input/rapor-input.js'
-  },
+'rapor-pbj-input-internal': {
+  title: 'Input Rapor PBJ',
+  subtitle: 'Form internal pegawai untuk input dan upload dokumen Rapor PBJ.',
+  type: 'iframe',
+  url: 'https://script.google.com/macros/s/AKfycbx7r228pPRdeO6egj_6bDsJu0-V4TY64XiQOG0sZCjhTLexaUV-oqk3PJCKpc3oSsIbTA/exec?embed=1'
+},
+
+'rapor-pbj-qc-internal': {
+  title: 'QC Rapor PBJ',
+  subtitle: 'Panel internal QC untuk review dan persetujuan rapor.',
+  type: 'iframe',
+  url: 'https://script.google.com/macros/s/AKfycbx7r228pPRdeO6egj_6bDsJu0-V4TY64XiQOG0sZCjhTLexaUV-oqk3PJCKpc3oSsIbTA/exec?page=qc&embed=1'
+}
 
   'rapor-pbj-qc-internal': {
     title: 'QC Rapor PBJ',
