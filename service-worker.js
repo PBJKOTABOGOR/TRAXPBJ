e30ec77215d0c2085501c561cdb13d67e1818443
PBJKOTABOGOR <pbjkotabogor@gmail.com>
2026-06-28 16:16:31 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,17 @@
-const CACHE_NAME = 'sippbj-pwa-v1';
-const ASSETS = ['./','./index.html','./mobile-pin-widget.css','./mobile-pin-widget.js','./manifest.json'];
+const CACHE_NAME = 'pbj-opd-widget-v1';
+const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.json','./favicon.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
-  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-  self.clients.claim();
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(()=>{});
+    return res;
+  }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html'))));
 });
